c36324f42bb59f1c415954bcc818d9b62f0c9033
qqer <wonters1223@gmail.com>
2026-01-21 10:11:01 +0200

    test(monobank): Fixed old tests and added new ones

--- tests/test_monobank.py
+++ tests/test_monobank.py
@@ -1,16 +1,18 @@
 from decimal import Decimal
 
 import pytest
+import requests
 
 from finmate.monobank.service import MonobankService
 from finmate.models import Users, Category
 from finmate.exceptions import ThrottlingError
+from finmate.exceptions import BusinessLogicError
 
 
 
 
 
-FAKE_CLIENT_INFO = {"accounts": [{"id": "ACC_ID_001"}]}
+FAKE_CLIENT_INFO = {"accounts": [{"id": "ACC_ID_001", 'type': 'black'}]}
 FAKE_MONO_TRANSACTIONS = [
     {"id": "mono_id_NEW_1", "amount": 10000, "description": "Sushi", "time": 1700000000, "mcc": 5812},
     {"id": "mono_id_OLD_2", "amount": 5000, "description": "Rent", "time": 1700000000, "mcc": 4900},
@@ -21,25 +23,25 @@ FAKE_USER = Users(id=1, monobank_api_token=b'FAKE_TOKEN')
 
 def setup_mocks(mocker):
     mock_mono_client = mocker.patch(
-        "backend.finmate.monobank.service.MonoAPI",
+        "finmate.monobank.service.MonoAPI",
         autospec=True
     ).return_value
     mock_mono_client.get_client_info.return_value = FAKE_CLIENT_INFO
 
     mock_profile_repo = mocker.patch(
-        "backend.finmate.monobank.service.ProfileRepository",
+        "finmate.monobank.service.ProfileRepository",
         autospec=True
     ).return_value
     mock_profile_repo.get_user_info.return_value = FAKE_USER
 
     mock_cat_repo = mocker.patch(
-        "backend.finmate.monobank.service.CategoryRepository",
+        "finmate.monobank.service.CategoryRepository",
         autospec=True
     ).return_value
     mock_cat_repo.get_by_name_and_user.return_value = FAKE_UNCAT_CATEGORY
 
     mock_tx_repo = mocker.patch(
-        "backend.finmate.monobank.service.TransactionRepository",
+        "finmate.monobank.service.TransactionRepository",
         autospec=True
     ).return_value
 
@@ -74,7 +76,6 @@ def test_sync_tx_success(mocker, app):
 
 
 def test_sync_tx_fails_on_rate_limit(mocker, app):
-
     mock_mono_client, mock_tx_repo, mock_cat_repo, mock_profile_repo = setup_mocks(mocker)
 
     RATE_LIMIT_ERROR = {"errorDescription": "Too many requests", "status_code": 429}
@@ -92,4 +93,44 @@ def test_sync_tx_fails_on_rate_limit(mocker, app):
     mock_tx_repo.bulk_insert_transactions.assert_not_called()
     mock_tx_repo.get_existing_mono_ids.assert_not_called()
 
-#TODO: Тест на помилку токена, Тест на помилку Monobank API, Тест на Empty Result
+
+def test_sync_tx_no_new_tx(mocker, app):
+    mock_mono_client , mock_tx_repo, mock_cat_repo, mock_profile_repo = setup_mocks(mocker)
+
+    mock_mono_client.get_transactions.return_value = {}
+    mock_tx_repo.bulk_insert_transactions.return_value = 0
+
+    service = MonobankService()
+
+    with app.app_context():
+        added_count = service.sync_tx(user_id=1)
+
+    assert added_count == 0
+    mock_tx_repo.bulk_insert_transactions.assert_not_called()
+
+def test_sync_tx_api_error(mocker, app):
+    mock_mono_client, mock_tx_repo, mock_cat_repo, mock_profile_repo = setup_mocks(mocker)
+    mock_mono_client.get_transactions.side_effect = requests.RequestException("Monobank is down")
+
+    service = MonobankService()
+
+    with pytest.raises(requests.RequestException):
+        with app.app_context():
+            service.sync_tx(user_id=1)
+
+    mock_tx_repo.bulk_insert_transactions.assert_not_called()
+
+
+def test_sync_tx_token_error(mocker, app):
+    mock_mono_client, mock_tx_repo, mock_cat_repo, mock_profile_repo = setup_mocks(mocker)
+
+    no_token_user = Users(id=1, monobank_api_token=None)
+    mock_profile_repo.get_user_info.return_value = no_token_user
+
+    service = MonobankService()
+
+    with pytest.raises(BusinessLogicError, match="API token not found or user access denied."):
+        with app.app_context():
+            service.sync_tx(user_id=1)
+
+    mock_tx_repo.bulk_insert_transactions.assert_not_called()
