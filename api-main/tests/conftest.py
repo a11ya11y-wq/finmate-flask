12c15f2e7cf0f811dce1d7d0a823ae6e676d0c04
qqer <wonters1223@gmail.com>
2026-07-04 18:41:29 +0300

    test(reporting): integrate allure-pytest and annotate base fixtures

--- api-main/tests/conftest.py
+++ api-main/tests/conftest.py
@@ -1,19 +1,27 @@
 import os
 
 import pytest
+import allure
 from core_service import create_app, db
 
 
 os.environ['FLASK_CONFIG'] = 'testing'
 
 @pytest.fixture(scope='session')
+@allure.title("Initializing a Test Flask Application (Session)")
 def app():
-    app = create_app(config_name='testing')
-    app.config['REDIS_URL'] = os.environ.get('REDIS_URL', 'redis://localhost:6379/1')
-    with app.app_context():
-        yield app
+
+    with allure.step("Creating Flask application for testing"):
+        app = create_app(config_name='testing')
+        app.config['REDIS_URL'] = os.environ.get('REDIS_URL', 'redis://localhost:6379/1')
+
+    with allure.step("Setting up the application context"):    
+        with app.app_context():
+            yield app
 
 
 @pytest.fixture(scope='session')
+@allure.title("Creating a Test Client")
 def client(app):
-    return app.test_client()
+    with allure.step("Creating a test client for the Flask application"):
+        return app.test_client()
